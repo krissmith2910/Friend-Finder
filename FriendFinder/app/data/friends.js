d593c79d3e72f42e6d94022a51bb7fbda424c0ac
Kristopher Smith <ksmith2910@outlook.com>
2019-10-15 18:53:38 -0600

    Updated Files

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,30 @@
+var user1 = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+var friends = [
+ { name: "sid", scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1] },
+ { name: "nancy", scores: [5, 1, 5, 5, 1, 1, 1, 1, 4, 2] }
+];
+var closestMatchVal = null;
+var closestMatchFriend = null;
+//console.log(user1[0]);
+for (var i = 0; i < friends.length; i++) {
+ var scores = friends[i].scores
+ var valDiffs = []
+ // let valDiffTotal = friends[i].scores
+ //   .map((score, j) => Math.abs(score - user1[j]))
+ //   .reduce((runTotal, nextVal) => runTotal + nextVal, 0);
+ for (j = 0; j < scores.length; j++) {
+   valDiffs.push(Math.abs(scores[j] - user1[j]));
+ }
+ console.log(valDiffs);
+ var valDiffTotal = valDiffs.reduce(function(a,b){return a + b},0);
+ console.log(valDiffTotal);
+ if (!closestMatchVal) {
+   closestMatchVal = valDiffTotal;
+   closestMatchFriend = i;
+ } else if (valDiffTotal < closestMatchVal) {
+   closestMatchVal = valDiffTotal;
+   closestMatchFriend = i;
+ }
+}
+console.log(friends[closestMatchFriend].name);
+console.log(closestMatchVal);
---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+
+  //app.post("/api/friends", function(req, res) {
+
+    //var newCharacter = req.body;
+  
+   
+   // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+   // console.log(newCharacter);
+  
+    //characters.push(newCharacter);
+  
+    //res.json(newCharacter);
+ // });

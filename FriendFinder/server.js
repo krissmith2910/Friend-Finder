1c506e5541beefedb6a0a28a843efc3c07e18420
Kristopher Smith <ksmith2910@outlook.com>
2019-10-20 20:18:58 -0600

    updated all files

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -9,14 +9,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//require("./app/data/friends.js")(app);
+
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-<<<<<<< HEAD
-//require("./app/data/friends.js")(app);
-=======
->>>>>>> 5da9fca25e5d6f316ebc9aa04666516f7d6cc0fb
+
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);

22c0300e8a2dffbfef8ad68bf8feb82aba1b980f
Kristopher Smith <ksmith2910@outlook.com>
2019-10-20 19:56:29 -0600

    Finished updating all files

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,17 +1,52 @@
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
+var friends = require("../data/friends");
+
+// ROUTING
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
-  //app.post("/api/friends", function(req, res) {
 
-    //var newCharacter = req.body;
+  app.post("/api/friends", function(req, res) {
+ 
+    var bestMatch = {
+      name: "",
+      friendDifference: Infinity
+    };
+
   
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    var totalDifference;
+
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      totalDifference = 0;
+
+      console.log(currentFriend.name);
+
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+     
+      if (totalDifference <= bestMatch.friendDifference) {
    
-   // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-  
-   // console.log(newCharacter);
-  
-    //characters.push(newCharacter);
-  
-    //res.json(newCharacter);
- // });
+        bestMatch.name = currentFriend.name;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+
+   
+    friends.push(userData);
+
+
+    res.json(bestMatch);
+  });
+};
--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -1,7 +1,14 @@
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
+var path = require("path");
+
+module.exports = function(app) {
+
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+};
